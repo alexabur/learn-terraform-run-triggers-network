18f045e972a3a2fb6c43a2b04b44ad976f213907
Alex Burak <alexabur@users.noreply.github.com>
2020-11-29 12:23:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+data "terraform_remote_state" "network" {
+  backend = "remote"
+
+  config = {
+    organization = var.tfc_org_name
+    workspaces = {
+          name = var.tfc_network_workspace_name
+    }
+  }
+}
+
 provider "aws" {
   version = "~> 2.7"
   region  = var.aws_region
